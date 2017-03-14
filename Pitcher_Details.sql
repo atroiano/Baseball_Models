2faf63dfd2607605dd86ebc6ba097a98f8432b9c
Andy Troiano <andytroiano@Andys-MBP.attlocal.net>
2017-03-13 20:01:31 -0400

    Initial Load
    Includes IP, Outs, Total Batters Faced and Pitches

---
+++ Pitcher_Details.sql
@@ -0,0 +1,17 @@
+SELECT 
+Pit_ID
+,case when BAT_HOME_ID = 1 then AWAY_TEAM_ID else HOME_TEAM_ID end as Team
+,sum(PA_BALL_CT+PA_CALLED_STRIKE_CT+PA_SWINGMISS_STRIKE_CT+PA_FOUL_STRIKE_CT+PA_INPLAY_STRIKE_CT+PA_OTHER_STRIKE_CT) as TotalPitches
+,sum(EVENT_OUTS_CT)/3 as InningsPitched
+,count(bat_id) as BattersFaced
+,substring( game_id, 4, 8 ) as date  
+FROM baseball.events 
+where HOME_TEAM_ID = 'PHI' 
+and cast(substring( game_id, 4, 8 ) as date)  >=  01-01-2015
+and game_id = 'PHI201004120'
+group by 
+substring( game_id, 4, 8 ) 
+,HOME_TEAM_ID
+,AWAY_TEAM_ID
+,Pit_ID
+,case when BAT_HOME_ID = 1 then AWAY_TEAM_ID else HOME_TEAM_ID end 

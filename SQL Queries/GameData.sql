0aa6858509523370ce7b5054b6dc25f6b14f96d7
Andy Troiano <andytroiano@Andys-MBP.attlocal.net>
2017-03-13 21:29:38 -0400

    Flip flop the name of the saved game field
    Flip flop the name of the saved game field

--- SQL Queries/GameData.sql
+++ SQL Queries/GameData.sql
@@ -19,7 +19,7 @@ g.GAME_ID
 ,g.Home_err_Ct
 ,g.away_lob_ct
 ,g.home_lob_ct
-,case when g.save_pit_id is not null then 'yes' else 'no' end as wasthereasave
+,case when g.save_pit_id is not null then 'yes' else 'no' end as SavedGame
 FROM baseball.games g
 left join baseball.lkup_cd_park_wind_direction wd on wd.VALUE_CD = g.WIND_DIRECTION_PARK_CD
 WHERE game_id = 'PHI201004120'
